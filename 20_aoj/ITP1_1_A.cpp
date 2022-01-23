8868cc74fd43abf054b1570d7a395234c468c093
Akira Kanai <kanai@wide.ad.jp>
2022-01-24 01:08:31 +0900

    s

---
+++ 20_aoj/ITP1_1_A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <chrono>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+using ll = long long int;
+
+#define FOR(i, begin, end) for(int i=(begin),i##_end_=(end);i<i##_end_;i++)
+#define REP(i, n) FOR(i,0,n)
+
+#define FASTIOpre() cin.tie(0); ios::sync_with_stdio(false); cout << fixed << setprecision(20);
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main() {
+    cout << "Hello World" << "\n";
+}
