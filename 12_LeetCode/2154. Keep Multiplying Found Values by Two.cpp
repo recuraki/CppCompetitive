52c56103c1ace65263a413284aae971d642fc327
Akira Kanai <kanai@wide.ad.jp>
2022-02-23 13:31:33 +0900

    s

---
+++ 12_LeetCode/2154. Keep Multiplying Found Values by Two.cpp
@@ -0,0 +1,4 @@
+//
+// Created by kanai on 2022/01/30.
+//
+
--- temp_chartSheet.cpp
+++ temp_chartSheet.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 
 int main(){
+    // nullptr
     // PQは大きい順
-    priority_queue<int, vector<int>, less<int>> pq;// と定義されている
+    priority_queue<int, vector<int>, less<int>> pq;// と定義されている。これは大きい分に取り出す。
     pq.emplace(1);
     pq.emplace(-1);
     cout << pq.top() << "\n"; pq.pop();
@@ -26,6 +27,18 @@ int main(){
     vector<int> nums={1,2,3};
     int maxval = *max_element(nums.begin(), nums.end());
 
+    deque<char> v2;
+    v2.push_back('a');
+    v2.push_back('1');
+    v2.push_back('2');
+    v2.push_front('z');
+    string s2(v2.begin(), v2.end());
+    cout << s2 << "\n";
+
+
     // IF false = 0 => while(1)だからねー
+
+#define ceil(a,b) (((a) + ((b) - 1)) / (b))
+    // string[index] はchar
 
 }
